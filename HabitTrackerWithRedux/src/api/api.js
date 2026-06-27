773fbac7d319e38bfdd24ed2b4a82149c13c445c
Suraj Bhavake <surajbhavake2@gmail.com>
2026-06-27 19:09:32 +0530

    Update API URL

--- HabitTrackerWithRedux/src/api/api.js
+++ HabitTrackerWithRedux/src/api/api.js
@@ -1,9 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:8000/api/",
+    // baseURL : "http://127.0.0.1:8000/api/",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
