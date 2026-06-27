e241be31c71420cb27bdc72983cd020daddb652b
Suraj Bhavake <surajbhavake2@gmail.com>
2026-06-27 19:49:59 +0530

    Update API URL

--- HabitTrackerWithRedux/src/api/api.js
+++ HabitTrackerWithRedux/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: "https://habitproject-production.up.railway.app/api/",
+    baseURL: "https://api.blackfade.site/api/",
 });
 
 export default api;
