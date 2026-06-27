888e9ab9ec8f6108a1c49acdacc1df1bda402454
Suraj Bhavake <surajbhavake2@gmail.com>
2026-06-27 19:22:16 +0530

    Update API URL

--- HabitTrackerWithRedux/src/api/api.js
+++ HabitTrackerWithRedux/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://habitproject-production.up.railway.app/api/",
 });
 
 export default api;
